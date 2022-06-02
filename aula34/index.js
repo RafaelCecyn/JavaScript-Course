356761ccc9c63da7c8e19f3c4342580a97a6ce9c
unknown <rafael.mendes112@hotmail.com>
2022-06-02 09:46:46 -0300

    Aula 34 e 35

---
+++ aula34/index.js
@@ -0,0 +1,33 @@
+
+// i - index
+// for (let i = 0; i <= 500; i++) {
+//     console.log(`Linha ${i}`);
+// }
+// for (let i = -10; i <= 500; i+=10) {
+//     console.log(`Linha ${i}`);
+// }
+
+// // Invertido
+// for (let i = 500; i >= 400; i -= 10) {
+//     console.log(`Linha ${i}`);
+// }
+
+// // Par
+// for (let i = 0; i <=10 ; i++) {
+//     const par = i % 2 === 0 ? `Par` : `Ímpar`;
+//     console.log(i, par)
+// }
+
+// // Impar
+// for (let i = 0; i <=10 ; i++) {
+//     const impar = i % 2 !== 0;
+//     console.log(i, impar)
+// }
+
+//               0       1      2
+const frutas = [`Maçã`,`Pera`,`Uva`]
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i])
+}
+
