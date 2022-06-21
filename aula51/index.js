66a6428ddcf2ac9dc3fb741ad0ab2213d660b1a9
unknown <rafael.mendes112@hotmail.com>
2022-06-20 21:49:44 -0300

    Aula sobre return

---
+++ aula51/index.js
@@ -0,0 +1,28 @@
+// return
+// retorna um valor
+// Termina a função
+
+// function soma(a,b) {
+//     return a + b;
+// }
+
+// function soma2(a,b) {
+//     console.log(a+b)
+// }
+
+// soma2(5,2);
+
+function criaMultiplicador (multiplicador) {
+    return function(n) {
+        return n * multiplicador;
+    };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadruplica = criaMultiplicador(4);
+
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadruplica(2));
+
